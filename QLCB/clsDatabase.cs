e0d28061258c435e203f702679b7b784c094ed82
Qismeeee <leanhquana3bff@gmail.com>
2025-02-11 10:21:42 +0700

    Create a QLCB Form

---
+++ QLCB/clsDatabase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace prjDVD
+{
+    class clsDatabase
+    {
+        public static SqlConnection con;
+
+        public static bool OpenConnection()
+        {
+            try
+            {
+                con = new SqlConnection("Server=Qismeeee; Database=QLCB;Integrated Security=True");
+                con.Open();
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool CloseConnection()
+        {
+            try
+            {
+                con.Close();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
